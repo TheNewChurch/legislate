09293eb8becc71a13501fd95c6304ce274a18d82
Emmanuel Owolabi <emmanuel.owolabi@mtc.co.uk>
2023-06-07 18:22:20 +0100

    sign-up

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
 import Link from 'next/link'
 import style from "../styles/Home.module.scss"
+import useEventbrite from 'react-eventbrite-popup-checkout';
+import { useCallback } from 'react';
 
 export default function Home() {
+  const handleOrderCompleted = useCallback(() => {
+    console.log('Order was completed successfully');
+  }, []);
+
+  const modalButtonCheckout = useEventbrite({
+    eventId: '632007019007',
+    modal: true,
+    onOrderComplete: handleOrderCompleted,
+  });
   return (
     <main className="main">
       <div className='homeWrapper container'>
@@ -10,6 +22,11 @@ export default function Home() {
             <div className={`page-container ${style.landingPage}`}>
               <h2 className={`text-white ${style.welcomeText}`}>Welcome to The Next Conference 2023</h2>
               <Link href={"/login"}> <button className='btn'>Sign In</button></Link>
+              {modalButtonCheckout && (
+                <button id={modalButtonCheckout.id} className='btn'>
+                  SignUp
+                </button>
+              )}
             </div>
       </div>
     </main>
