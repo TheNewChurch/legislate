9f01ef4077aa150ba68b2474f6cc23d463b965cf
Emmanuel Owolabi <emmanuel.owolabi@mtc.co.uk>
2023-06-08 17:09:28 +0100

    login and loader fix

--- components/login/loginForm.tsx
+++ components/login/loginForm.tsx
@@ -1,6 +1,7 @@
 import style from '../../styles/Login.module.scss'
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { loginFunction } from '@/types';
+import Link from 'next/link';
 
 type props= {
     onSuccessful: loginFunction
@@ -9,42 +10,68 @@ type props= {
 export default function LoginForm({onSuccessful}:props){
 
     const [email,setEmail] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const [errorMessage,setErrorMessage] = useState({
+        message: '',
+        showSignUp: false
+    }) 
+
 
     async function submit(){
         
         const url = `https://www.eventbriteapi.com/v3/events/632007019007/orders/?expand=attendees&only_emails=${email}`
-        try {
-            const {orders} = await fetch(url,{
-                method: 'GET',
-                headers: {
-                    authorization: "Bearer OXDFBPLUQWPVZN2CQI7K"
-                }
-            }).then(resp=>resp.json());
+        setErrorMessage({
+            message: '',
+            showSignUp: false
+        })
+        if(!isLoading) {
             
-            if(orders) {
-                const {
-                    attendees: [
-                        {
-                            barcodes : [
-                                {
-                                  barcode = ""  
-                                } = {}
-                            ] = []
-                        } = {}
-                    ] = []
-                } = orders.find((item:{email:string,id:string})=>item.email === email)
-
-                console.log(barcode)
-                if(barcode) {
-                    
-                     onSuccessful({success:true,barcode})
+            try {
+                setIsLoading(true)
+                const {orders} = await fetch(url,{
+                    method: 'GET',
+                    headers: {
+                        authorization: "Bearer OXDFBPLUQWPVZN2CQI7K"
+                    }
+                }).then(resp=>resp.json());
+                
+                setIsLoading(false)
+    
+                if(orders) {
+                    const {
+                        attendees: [
+                            {
+                                barcodes : [
+                                    {
+                                      barcode = ""  
+                                    } = {}
+                                ] = []
+                            } = {}
+                        ] = [{}]
+                    }  = orders.find((item:{email:string,id:string})=>item.email === email) || {}
+    
+                    if(barcode) {    
+                          onSuccessful({success:true,barcode})
+                          return;
+                    }
                 }
+               
+                setErrorMessage(
+                    {
+                        message:"Your email doesn't exist in our database. if you wish to register ",
+                        showSignUp:true
+                    }
+                )
+                
+            } catch(e){
+                setIsLoading(false)
+                
+                
+                setErrorMessage(prev=>({...prev,message: "there is an error fetching your data please try again later"}))
+                console.log(e)
             }
-           
-            
-        } catch(e){
-            console.log(e)
         }
+        
         
         
     }
@@ -65,7 +92,28 @@ export default function LoginForm({onSuccessful}:props){
                     </div>     
                     <span className='formHints'>This should be the email you used to purchase your ticket</span>
                 </div>
-                <button onClick={submit} className={`btn ${style.confirmButton}`}> Confirm Email</button>             
+                {
+                    errorMessage &&
+                    <p className={`textError ${style.loginError}`}> 
+                        { 
+                            errorMessage.message
+                        } 
+                        
+                        {
+                        errorMessage?.showSignUp &&
+                        <Link href="/signup">SignUp</Link>
+                        }
+                    
+                    </p>
+                }
+                
+
+                <button onClick={submit} className={`btn ${style.confirmButton}`}> 
+                    Confirm Email 
+                    {
+                        isLoading && <div className='loading'></div>
+                    }
+                </button>             
             </div>
         </div>
     )
